fc7635d8716db2777c16fca1d460c9d2c96824a4
Elid12 <122638713+Elid12@users.noreply.github.com>
2023-01-13 21:37:28 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Code for number operations");
+        System.out.println("Type 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division");
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+
+
+    }
+
+}
